77503b188c44fd0bcb4946c7cf7e1a332f62d593
kasi <kasionaga@gmail.com>
2015-09-26 10:14:07 -1000

    19! Im not really a fan of beers

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -279,6 +279,14 @@ var scmList = ['git', 'svn', 'mercurial', 'bazaar', 'cvs'];
  *
  */
 
+var beers = {
+  IPA : 'Ale',
+  Lager : 'Strong',
+  Heffeweisen : 'German',
+  Stout : ['Thick', 'Dark'],
+  Porter : 'Bitter',
+  Ale : ['Light', 'Golden']
+};
 
 /* Step 20
  *
