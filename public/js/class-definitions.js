94a297a336db9ce6e29dc1c6290383753c466035
kasi <kasionaga@gmail.com>
2015-09-26 14:40:01 -1000

    33! why is luke gross?

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -642,7 +642,31 @@ SolarSystem.prototype.removePlanet = function(planet) {
  *
  */
 
+function PrincessLeia(name, money, age, gender, isInTrouble) {
+  Person.call(this, name, money, age, gender);
+  this.isInTrouble = null;
+}
+PrincessLeia.prototype = Object.create(Person.prototype, {
+  constructor : {
+    value : PrincessLeia
+  }
+});
 
+PrincessLeia.prototype.shootsGun = function() {
+  isInTrouble = false;
+  return 'Leia shoots her gun wildly';
+};
+PrincessLeia.prototype.getsInTrouble = function() {
+  isInTrouble = true;
+  return 'Help me Obi-wan Kenobi, you\'re my only hope';
+};
+PrincessLeia.prototype.marries = function(lover) {
+  if (lover === 'Han Solo') {
+    return true;
+  } else if (lover === 'Luke Skywalker') {
+    return 'Gross!';
+  };
+};
 /* Step 34
  *
  * Define a class named "Stapler" with properties "color"
